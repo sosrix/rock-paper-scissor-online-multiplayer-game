8a0a429023d0ee3d61ec58effe550a57be84b47e
Soufiyan <ahminsoufiyan@gmail.com>
2022-08-14 23:46:09 +0100

    Seperating the code to more components

---
+++ src/userArea.js
@@ -0,0 +1,20 @@
+export default function userArea({ user }) {
+  return (
+    <div className="userArea">
+      <div className="user">
+        <div className="avatar">
+          <img
+            className="avatar__image"
+            alt="myAvatar"
+            src="https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png"
+          />
+        </div>
+        <p>{user ? user.email.toUpperCase() : "USERNAME?"}</p>
+        <button className="singOut-btn" onClick={logout}>
+          Sign Out
+        </button>
+      </div>
+      <p className="userMessage"> SCORE : {previousScore}</p>
+    </div>
+  );
+}
